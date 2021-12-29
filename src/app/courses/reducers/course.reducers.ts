62d060f5f32faded5b99c5db234d47ddb035eab6
Michelle Roberts <michelleroberts5@gmail.com>
2021-12-29 14:16:55 -0500

    S5-30: Implementing reducers and adapter to store entities State

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,4 +1,19 @@
-import { EntityState } from "@ngrx/entity";
-import { Course } from "../model/course";
+import { createEntityAdapter, EntityState } from "@ngrx/entity";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../action-types";
+import { Course, compareCourses } from "../model/course";
 
-export interface CoursesState extends EntityState<Course> {}
+export interface CoursesState extends EntityState<Course> {
+
+}
+
+// Adapters are
+export const adapter = createEntityAdapter<Course>();
+
+export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+    initialCoursesState,
+    on(CourseActions.allCoursesLoaded,
+        (state, action) => adapter.addAll(action.courses, state))
+);
