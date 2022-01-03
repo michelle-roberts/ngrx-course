1ae74c613244e6f4c4138d5231ee2c91187550a1
Michelle Roberts <michelleroberts5@gmail.com>
2022-01-03 14:37:46 -0500

    S6-41 - Controlling data loading with NgRx data loaded flagin the route resolver file

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { first, tap, filter } from "rxjs/operators";
 import { CourseEntityService } from "./course-entity.service";
 
 @Injectable() 
@@ -9,9 +9,15 @@ export class CoursesResolver implements Resolve<boolean> {
     constructor(private coursesService: CourseEntityService) {}
     
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.coursesService.getAll()
+        return this.coursesService.loaded$
             .pipe(
-                map(courses => !!courses)
+               tap(loaded => {
+                    if (!loaded) {
+                        this.coursesService.getAll();
+                    }
+                }),
+                filter(loaded => !!loaded),
+                first()
             );
     }
 }
