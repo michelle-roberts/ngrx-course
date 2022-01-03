697821815c30e95a2af9ef6ee83c36a25aae9100
Michelle Roberts <michelleroberts5@gmail.com>
2022-01-03 14:07:53 -0500

    S6-39 - setting up NGRX Data

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { CourseEntityService } from "./course-entity.service";
 
 @Injectable() 
@@ -8,6 +9,9 @@ export class CoursesResolver implements Resolve<boolean> {
     constructor(private coursesService: CourseEntityService) {}
     
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return; 
+        return this.coursesService.getAll()
+            .pipe(
+                map(courses => !!courses)
+            );
     }
 }
